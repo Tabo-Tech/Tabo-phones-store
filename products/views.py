2e55bd6c95558f56103c89576f1e50cbdd6c50d4
Tabo-Store <hashimyg583@gmail.com>
2025-03-22 08:42:44 -0700

    Added missing import for path

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
-from django.http import JsonResponse, HttpResponse
+from django.urls import path
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Product
 
-@csrf_exempt
+@csrf_exempt  # Inaruhusu requests hata kama hazina CSRF token
 def product_list(request):
     if request.method == "GET":
         products = list(Product.objects.values())  # Rudisha data kama JSON
@@ -13,5 +14,7 @@ def product_list(request):
 
     return JsonResponse({"error": "Method Not Allowed"}, status=405)
 
+from django.http import HttpResponse
+
 def home(request):
-    return HttpResponse("Welcome to Tabostore!")
+    return HttpResponse("Welcome to Tabostore!")  # Au unaweza kuredirect kwenye 'products/'
