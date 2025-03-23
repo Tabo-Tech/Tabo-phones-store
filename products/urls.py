7a93348ca6972e33b76b6ee0014036f2eb6814f3
Tabo-Store <hashimyg583@gmail.com>
2025-03-23 08:04:15 -0700

    Updated urls.py to include home route and fixed product list view

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from products import views  # Hakikisha views zako zipo
+from django.shortcuts import render
+from .models import Product
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.home, name="home"),
-    path('products/', views.product_list, name='product_list'),  # Route ya kuonyesha bidhaa
-]
-
-# Ruhusu Django kuhudumia picha kwenye mode ya development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+def product_list(request):
+    products = Product.objects.all()  # Fetch all products
+    return render(request, 'products/product_list.html', {'products': products})
